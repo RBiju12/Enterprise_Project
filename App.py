15e3431499a2a9e68702e9ee76e4f2b89b2cd24b
Rishan Biju <rishanbiju@gmail.com>
2023-03-16 09:17:03 -0400

    Parsed the data the return the top ten seeds

--- App.py
+++ App.py
@@ -7,18 +7,24 @@ app = Flask(__name__, template_folder='Templates')
 def home():
     return render_template('index.html')
 
-@app.route("/weather")
-def fetchweather():
-    with open('2018.json') as file:
+@app.route("/marchmadness")
+def fetchmbasketball():
+    with open("2018.json") as file:
         data = json.load(file)
-    
-    top_10 = sorted(data['teams'], key=lambda x: x['seed'])[:10]
+
+    game = []
+    top_10 = sorted(data["team"][:10])
+    for teams in top_10:
+        for place in data["seed"]:
+            if place > 10:
+                return teams
+    game.append(teams)
+
     if request.method == 'POST':
         selected_team = request.form.get('team')
         return f"You selected {selected_team}."
     else:
         return render_template('marchmadness.html', teams=top_10)
-
 
 if __name__ == '__main__':
     app.run(host="localhost", port=8000, debug=True)
