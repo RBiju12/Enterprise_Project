79de481fd4ee002a1f9c7e1ffcfc985e9b627a4c
Rishan Biju <rishanbiju@gmail.com>
2023-03-17 09:25:41 -0400

    Finished 2nd chart

--- App.py
+++ App.py
@@ -10,7 +10,7 @@ def home():
     return render_template("Crypto.html")
 
 @app.route('/chart', methods=['GET'])
-def charts():
+def chart1():
     url = "https://fcsapi.com/api-v3/crypto/supply?symbol=BTC,XRP&convert=USD&access_key=YJ0kxYoB0J79xzs4htS8K5BI8"
 
     data = requests.get(url)
@@ -24,8 +24,8 @@ def charts():
         y_values.append(item["price"])
     
     plt.plot(x_values, y_values)
-    plt.xlabel('X Values')
-    plt.ylabel('Y Values')
+    plt.xlabel('Daily Percent Change')
+    plt.ylabel('Price')
     plt.title('API Data Plot')
     plt.show()
 
@@ -34,6 +34,31 @@ def charts():
     plt.savefig(plot_path)
 
     return f'<img src="{plot_path}">'
+
+def chart2():
+    url = "https://fcsapi.com/api-v3/crypto/supply?symbol=BTC,XRP&convert=USD&access_key=YJ0kxYoB0J79xzs4htS8K5BI8"
+    data = requests.get(url)
+    cryptodatav2 = data.json()
+
+    x_axis = []
+    y_axis = []
+
+    for val in cryptodatav2:
+        x_axis.append(val["percentage_change_7d"])
+        y_axis.append(val["market_cap"])
+
+    plt.plot(x_axis, y_axis)
+    plt.xlabel("Weekly percent change")
+    plt.ylabel("Market Cap")
+    plt.title("Weekly Percentage change vs Market_Cap")
+    plt.show()
+
+    plot_path2 = os.path.join(app.static_folder, 'plot2.png')
+    plt.savefig(plot_path2)
+
+
+    return f'<img src="{plot_path2}">'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
